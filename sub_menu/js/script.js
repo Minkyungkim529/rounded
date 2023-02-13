5ab072169a5adb4c7f64a329b6d9cd3a0c4dad49
Minkyungkim529 <121825593+Minkyungkim529@users.noreply.github.com>
2023-02-13 13:38:05 +0900

    Add files via upload

--- sub_menu/js/script.js
+++ sub_menu/js/script.js
@@ -5,9 +5,18 @@ $(function(){
 });
 //하트 찜하기 버튼
 
+// $(function(){
+//     $(".off").click(function(){
+//         $(this).removeClass('off').addClass("on").siblings('.on').removeClass('on').addClass('off');
+//     });
+// });
+// //hot, iced버튼 토글
+
 $(function(){
-    $(".off").click(function(){
-        $(this).removeClass('off').addClass("on").siblings('.on').removeClass('on').addClass('off');
+    //.board li a를 누르면
+    $('.coffee li .hi').click(function(){
+    //눌러진 a의 부모요소에 'on'클래스를 추가하고,
+    //부모의 형제요소들은 .on을 지운다.
+        $(this).parent().addClass('mbtn_on').siblings().removeClass('mbtn_on');
     });
 });
-//hot, iced버튼 토글
