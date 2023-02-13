dc4b76b39d7c7df014d91b57ff9421eb13ad113e
Minkyungkim529 <121825593+Minkyungkim529@users.noreply.github.com>
2023-02-13 13:33:11 +0900

    Add files via upload

--- sub_brand/js/script.js
+++ sub_brand/js/script.js
@@ -1,3 +1,12 @@
 $(function(){
 	AOS.init();	
 });
+
+$(function(){
+    //.board li a를 누르면
+    $('.character_tab li a').click(function(){
+    //눌러진 a의 부모요소에 'on'클래스를 추가하고,
+    //부모의 형제요소들은 .on을 지운다.
+        $(this).parent().addClass('cbtn_on').siblings().removeClass('cbtn_on');
+    });
+});
